501a74aee79e15b96b6725ef1b8cb32301dcae0e
Fuad <kamilovfuad@gmail.com>
2022-06-04 12:09:35 +0400

    Add routes and controller for book model

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Book;
+use Illuminate\Support\Facades\Auth;
 
 class BookController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('books.books');
+    }
+
+    public function create()
+    {
+        return view('books.create');
+    }
+
+    public function store(Request $request, Book $book)
+    {
+        $book->name = $request['name'];
+        $book->description = $request['description'];
+        $book->price = $request['price'];
+        // $book->user_id = Auth::user()->id;
+    }
 }
--- database/migrations/2022_06_03_054332_create_books_table.php
+++ database/migrations/2022_06_03_054332_create_books_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('name');
             $table->float('price');
             $table->text('description');
-            $table->unsignedBigInteger('user_id');
+            // $table->unsignedBigInteger('user_id');
 
             $table->timestamps();
         });
