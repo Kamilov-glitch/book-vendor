3e2042a52f4c614fd67a6a64630e18599bcab145
Fuad <kamilovfuad@gmail.com>
2022-06-22 10:31:07 +0400

    Work on books view a bit

--- resources/views/books/books.blade.php
+++ resources/views/books/books.blade.php
@@ -3,12 +3,15 @@
 
 <div class="container">
     @foreach (App\Models\Book::all() as $book)
-        <div class="card">
-            <div class="card-header">
-                {{ $book->name }}
-            </div>
-            <div class="card-body">
-                {{ $book->description }}
+        <div class="col-4 pb-3">
+            <div class="card">
+                <img src="" alt="book image" class="card-img-top">
+                <div class="card-header">
+                    {{ $book->name }}
+                </div>
+                <div class="card-body">
+                    {{ $book->description }}
+                </div>
             </div>
         </div>
         
