a5c96823a1c09f5773c401102b39a99611e3c5ec
Fuad <kamilovfuad@gmail.com>
2022-06-24 10:39:40 +0400

    do barely anything

--- resources/views/books/books.blade.php
+++ resources/views/books/books.blade.php
@@ -5,12 +5,20 @@
     @foreach (App\Models\Book::all() as $book)
         <div class="col-4 pb-3">
             <div class="card">
-                <img src="" alt="book image" class="card-img-top">
                 <div class="card-header">
-                    {{ $book->name }}
+                    <img src="" alt="book image" class="card-img-top w-100">
                 </div>
-                <div class="card-body">
-                    {{ $book->description }}
+                <div class="card-body text-center">
+                    <h5>{{ $book->name }}</h5>
+                    <div class="d-flex justify-content-center">
+                        <h6>{{$book->price}}</h6>
+                        <h6 class="text-muted ms-2">
+                            <del>{{$book->price}}</del>
+                        </h6>
+                    </div>
+                </div>
+                <div class="card-footer">
+                    <button class=""></button>
                 </div>
             </div>
         </div>
